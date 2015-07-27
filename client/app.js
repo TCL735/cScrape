5aa5768b9ca837389ddbf679ac34784e684d6241
Chris <cpa.staton@gmail.com>
2015-07-27 15:54:25 -0700

    remove extra slash in url

--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
 $(function() {
   $("button").on("click", function() {
     $.ajax({
-      url: "https://glacial-waters-2127.herokuapp.com//test",
+      url: "https://glacial-waters-2127.herokuapp.com/test",
       type: "GET",
       dataType: "json",
       success: function(data) {
