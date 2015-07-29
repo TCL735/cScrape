30160889168ccdfc55552cc0a27ea136f33dafd6
Chris <cpa.staton@gmail.com>
2015-07-28 17:09:41 -0700

    cron job

--- myServer.js
+++ myServer.js
@@ -1,4 +1,8 @@
 var express = require("express");
+var fs = require("fs");
+
+var CronJob = require("cron").CronJob;
+
 var app = express();
 
 
@@ -102,7 +106,7 @@ app.get("/test", function(req, res) {
 });
 
 
-app.get("/grabData", function(req, res) {
+app.get("/funTest", function(req, res) {
   phantom.create(function(ph) {
     ph.createPage(function(page) {
       page.open("http://blog.arisetyo.com/", function(status) {
@@ -119,6 +123,10 @@ app.get("/grabData", function(req, res) {
             }, function(result) {
               console.log(result);
               ph.exit()
+              fs.writeFile('data.js', JSON.stringify(result), function(err) {
+                if (err) throw err;
+                console.log("Data saved!");
+              })
               res.json(result);
             });
           }, 2000);
@@ -127,6 +135,117 @@ app.get("/grabData", function(req, res) {
     });
   });
 });
+
+app.get("/grabData", function(req, res) {
+  fs.readFile('data.js', 'utf8', function(err, data) {
+    res.json(JSON.parse(data));
+  });
+});
+
+
+
+var job = new CronJob({
+  cronTime: '00 11 17 * * 1-5',
+  onTick: function() {
+
+    phantom.create(function(ph) {
+      ph.createPage(function(page) {
+        page.open("https://angel.co/login?utm_source=top_nav_home", function(status) {
+          console.log("opened Page?", status);
+          page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js", function(){
+            setTimeout(function() {
+              return page.evaluate(function() {
+                //logging into account
+                $("#user_email").val("testme777@gmail.com");
+                $("#user_password").val("bobthebuilder");
+                $("input").last().click();
+
+              }, function(result1) {
+                console.log("Logged In");
+                setTimeout(function() {
+                  return page.evaluate(function() {
+                    $(".remove-filter.delete")[1].click();
+
+                  }, function(result2) {
+                    console.log("Removed Filter");
+                    setTimeout(function() {
+                      return page.evaluate(function() {
+                        var stopMe = setInterval(function() {
+                          document.body.scrollTop = document.body.scrollHeight;
+                        }, 1800);
+                        return stopMe;
+                      }, function(result3) {
+                        console.log("result3: ", result3);
+                        console.log("Scrolling Initiated");
+                        setTimeout(function() {
+                          return page.evaluate(function(result3) {
+
+                            clearInterval(result3);
+                          }, function(result4) {
+                            console.log("scrolling finished")
+
+                            setTimeout(function() {
+                              return page.evaluate(function() {
+
+                                $(".fbw9").click();
+
+
+                                var results = [];
+
+                                var jobsPerCompany = $(".details-row.jobs");
+
+                                for (var i = 0; i < jobsPerCompany.length; i++) {
+                                  results.push({
+                                    companyName: $(jobsPerCompany[i]).closest(".details").prev().prev().find("a").first().text(),
+                                    jobsPerCompany: []
+                                  });
+
+                                  var arrayOfJobsForIndividualCompany = $(jobsPerCompany[i]).find(".listing-row"); 
+
+                                  for (var j = 0; j < arrayOfJobsForIndividualCompany.length; j++) {
+                                    var jobFinder = $(arrayOfJobsForIndividualCompany[j]);
+                                    results[i].jobsPerCompany.push({
+                                      jobTitle: jobFinder.find("a").first().text(),
+                                      jobSalary: jobFinder.find(".compensation").text(),
+                                      jobURL: jobFinder.find("a").first().attr("href"),
+                                      jobInfo: jobFinder.find(".tags").text()
+                                    });
+                                  }
+                                }
+                                return JSON.stringify(results);                                        
+
+                              }, function(result5) {
+                                console.log(result5);
+                                ph.exit();
+                                fs.writeFile('data.js', JSON.stringify(result5), function(err) {
+                                  if (err) throw err;
+                                  console.log("Data saved!");
+                                })
+                              });  
+                            }, 3000);
+                          })
+                        }, 43333);
+                      });
+                    }, 2000)
+                  });
+                }, 2000);
+              });
+            }, 3000);
+          });
+        });
+      });
+    });
+
+
+  },
+  start: false,
+  timeZone: "America/Los_Angeles"
+});
+
+job.start();
+
+
+
 
 app.listen(app.get("port"), function() {
   console.log("Server is running!");
