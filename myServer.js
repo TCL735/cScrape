b0e0d21b92569ec42468ddc8ff7aac63370041aa
Chris <cpa.staton@gmail.com>
2015-07-29 20:05:17 -0700

    test

--- myServer.js
+++ myServer.js
@@ -145,7 +145,7 @@ app.get("/grabData", function(req, res) {
 
 
 var job = new CronJob({
-  cronTime: '00 04 20 * * 1-5',
+  cronTime: '00 08 20 * * 1-5',
   onTick: function() {
 
     phantom.create(function(ph) {
