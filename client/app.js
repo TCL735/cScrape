598b6e2f663943716663a0632ef4182d0c3bfa12
Chris <cpa.staton@gmail.com>
2015-08-06 16:34:22 -0700

    update

--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
 $(function() {
   $("button").on("click", function() {
     $.ajax({
-      url: "https://glacial-waters-2127.herokuapp.com/grabData",
+      url: "/grabData",
       type: "GET",
       dataType: "json",
       success: function(data) {
